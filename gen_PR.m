a161aa18d343682298486b091976da2d6163225f
unknown <adrielfilipe14@gmail.com>
2023-03-12 09:23:19 -0300

    daskjl

--- gen_PR.m
+++ gen_PR.m
@@ -10,6 +10,7 @@ function D = gen_PR(dataset)
 
     for i = 1:length(files)
         file_name = string(files(i).name);
+        dir_path
         res(i) = load(dir_path+file_name);
         error("dklasj")
     end
