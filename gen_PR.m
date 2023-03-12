4990ffbc0ba187de41457cc098d660c6d0ffd1e2
unknown <adrielfilipe14@gmail.com>
2023-03-12 09:48:08 -0300

    daskjl

--- gen_PR.m
+++ gen_PR.m
@@ -84,6 +84,6 @@ function D = gen_PR(dataset)
         end
 
         points = [TPR;PPV];
-        save(dir_path+"pr/"+file_names(ii)+"_pr"+".mat",'points')
+        save(dir_path+"pr/"+file_names(ii),'points')
     end
 end
