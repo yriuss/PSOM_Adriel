ad4ff00203217c634a1826badf9d192ef0a5e5a7
unknown <adrielfilipe14@gmail.com>
2023-03-12 08:57:33 -0300

    daskjl

--- gen_PR.m
+++ gen_PR.m
@@ -1,7 +1,8 @@
 % Adriel
 
 function D = gen_PR(dataset)
-    res = load("logs/results/"+dataset+"/multiple/"+"results_layer_.*_single_.*_fator_.*_multiple_.*_fator_.*.mat");
+    
+    res = load("logs/results/"+dataset+"/multiple/"+regexp("results_layer_.*_single_.*_fator_.*_multiple_.*_fator_.*.mat", 'once'));
 
     for i = 1:length(res)
         [~,best_idx] = max(res(i).Model.test.layer{2}.scoreTest);
